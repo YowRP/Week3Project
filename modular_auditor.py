9c72b7382567a484692e73089669f191ca46b0ec
Auditioning <aidenyow2006@gmail.com>
2026-09-18 15:36:32 +0800

    generate report altered

--- modular_auditor.py
+++ modular_auditor.py
@@ -22,8 +22,9 @@ def calculate_tax(amount):
     tax_rate = 0.1
     return amount * tax_rate
 
-def generate_report(total_units, failed_attempts):
+def generate_report(total_units, deliveries_processed, failed_attempts):
     print("Total units will be : " + str(total_units))
+    print("Deliveries processed : " + str(deliveries_processed))
     print("Failed attempts : " + str(failed_attempts))
 
 def main() :
